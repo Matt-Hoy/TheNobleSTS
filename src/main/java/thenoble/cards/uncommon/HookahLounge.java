23dc9f08c09cdb93518e9fc7fb3d9afd07e33cd0
Matt-Hoy <mattdhoy@gmail.com>
2025-02-27 11:13:08 -0600

    HookahLounge

---
+++ src/main/java/thenoble/cards/uncommon/HookahLounge.java
@@ -0,0 +1,44 @@
+package thenoble.cards.uncommon;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PoisonPower;
+import thenoble.cards.NobleCard;
+import thenoble.character.MyCharacter;
+import thenoble.util.CardStats;
+
+public class HookahLounge extends NobleCard {
+  public static final String ID = makeID("HookahLounge");
+  private static final CardStats INFO =
+      new CardStats(
+          MyCharacter.Meta.CARD_COLOR,
+          CardType.SKILL,
+          CardRarity.UNCOMMON,
+          CardTarget.ALL_ENEMY,
+          0);
+  private static final int MAGIC = 1;
+  private static final int UPG_MAGIC = 1;
+
+  public HookahLounge() {
+    super(ID, INFO);
+
+    setMagic(MAGIC, UPG_MAGIC);
+  }
+
+  @Override
+  public void use(AbstractPlayer player, AbstractMonster monster) {
+    for (AbstractMonster individualMonster : AbstractDungeon.getMonsters().monsters) {
+      addToBot(
+          new ApplyPowerAction(
+              individualMonster, player, new PoisonPower(individualMonster, player, magicNumber)));
+    }
+  }
+
+  @Override
+  public AbstractCard makeCopy() {
+    return new HookahLounge();
+  }
+}
