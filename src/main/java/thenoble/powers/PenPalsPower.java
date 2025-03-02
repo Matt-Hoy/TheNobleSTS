fbd4b5044b642f5c1411c899e2530f3b149c56b9
Matt-Hoy <mattdhoy@gmail.com>
2025-03-02 13:43:13 -0600

    Pen Pals

---
+++ src/main/java/thenoble/powers/PenPalsPower.java
@@ -0,0 +1,34 @@
+package thenoble.powers;
+
+import static thenoble.TheNoble.makeID;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class PenPalsPower extends BasePower {
+  public static final String POWER_ID = makeID("PenPals");
+  private static final PowerType TYPE = PowerType.BUFF;
+  private static final boolean TURN_BASED = false;
+
+  public PenPalsPower(AbstractCreature owner, int amount) {
+    super(POWER_ID, TYPE, TURN_BASED, owner, amount);
+  }
+
+  @Override
+  public void onCardDraw(AbstractCard card) {
+    addToBot(
+        new DamageAllEnemiesAction(
+            AbstractDungeon.player,
+            amount,
+            DamageInfo.DamageType.THORNS,
+            AbstractGameAction.AttackEffect.SLASH_HEAVY));
+  }
+
+  public void updateDescription() {
+    this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+  }
+}
