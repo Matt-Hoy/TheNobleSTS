3c11812f7cd4a334cb9217d21c8ed7794903b712
Matt-Hoy <mattdhoy@gmail.com>
2025-03-03 11:28:20 -0600

    Fancy Boots 1-2

--- src/main/java/thenoble/cards/common/FancyBoots.java
+++ src/main/java/thenoble/cards/common/FancyBoots.java
@@ -15,7 +15,7 @@ public class FancyBoots extends NobleCard {
   private static final CardStats INFO =
       new CardStats(
           MyCharacter.Meta.CARD_COLOR, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF, 1);
-  private static final int MAGIC = 0;
+  private static final int MAGIC = 1;
   private static final int UPG_MAGIC = 1;
 
   public FancyBoots() {
