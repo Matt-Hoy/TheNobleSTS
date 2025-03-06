fd002269388189f9a947c777bdc0440dfffdc5e7
Matt-Hoy <mattdhoy@gmail.com>
2025-03-05 18:40:30 -0600

    Distract

---
+++ src/main/java/thenoble/cards/uncommon/TalkOver.java
@@ -0,0 +1,48 @@
+package thenoble.cards.uncommon;
+
+import com.evacipated.cardcrawl.mod.stslib.actions.common.AllEnemyApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.ChokePower;
+import thenoble.cards.NobleCard;
+import thenoble.character.MyCharacter;
+import thenoble.util.CardStats;
+
+import static thenoble.cards.type.CachetCard.cachetAmount;
+
+public class TalkOver extends NobleCard {
+  public static final String ID = makeID("TalkOver");
+  private static final CardStats INFO =
+      new CardStats(
+          MyCharacter.Meta.CARD_COLOR, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF, 0);
+  private static final int BLOCK = 2;
+  private static final int UPG_BLOCK = 1;
+  private static final int MAGIC = 0;
+  private static final int UPG_MAGIC = 1;
+
+  public TalkOver() {
+    super(ID, INFO);
+
+    setBlock(BLOCK, UPG_BLOCK);
+    setMagic(MAGIC, UPG_MAGIC);
+  }
+
+  @Override
+  public void use(AbstractPlayer player, AbstractMonster monster) {
+    addToBot(new GainBlockAction(player, block));
+    if (magicNumber > 0 || cachetAmount() > 0) {
+      addToBot(
+          new AllEnemyApplyPowerAction(
+              player,
+              magicNumber,
+              (individualMonster) -> (new ChokePower(individualMonster, magicNumber))));
+    }
+  }
+
+  @Override
+  public AbstractCard makeCopy() {
+    return new TalkOver();
+  }
+}
