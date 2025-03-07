bee51f8986529593301204d5585c139a4aa520f1
Matt-Hoy <mattdhoy@gmail.com>
2025-03-06 20:17:55 -0600

    Updated / Improved Spectral Disposition

--- src/main/java/thenoble/actions/RandomCardFromExhaustPileToHandAction.java
+++ src/main/java/thenoble/actions/RandomCardFromExhaustPileToHandAction.java
@@ -5,6 +5,8 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
+import java.util.ArrayList;
+
 public class RandomCardFromExhaustPileToHandAction extends AbstractGameAction {
   private final AbstractPlayer player;
 
@@ -15,8 +17,13 @@ public class RandomCardFromExhaustPileToHandAction extends AbstractGameAction {
   }
 
   public void update() {
-    if (!player.exhaustPile.isEmpty()) {
-      AbstractCard card = player.exhaustPile.getRandomCard(AbstractDungeon.cardRandomRng);
+    if (exhaustIsValid()) {
+      ArrayList<AbstractCard> candidates = new ArrayList<>();
+      candidates.addAll(player.exhaustPile.getAttacks().group);
+      candidates.addAll(player.exhaustPile.getSkills().group);
+      candidates.addAll(player.exhaustPile.getPowers().group);
+      int rand = AbstractDungeon.cardRandomRng.random(0, candidates.size() - 1);
+      AbstractCard card = candidates.get(rand);
       player.hand.addToHand(card.makeCopy());
       player.exhaustPile.removeCard(card);
       player.hand.refreshHandLayout();
@@ -24,5 +31,12 @@ public class RandomCardFromExhaustPileToHandAction extends AbstractGameAction {
 
     tickDuration();
     isDone = true;
+  }
+
+  private boolean exhaustIsValid() {
+    return (!player.exhaustPile.isEmpty()
+        && (!player.exhaustPile.getSkills().isEmpty()
+            || !player.exhaustPile.getAttacks().isEmpty()
+            || !player.exhaustPile.getPowers().isEmpty()));
   }
 }
