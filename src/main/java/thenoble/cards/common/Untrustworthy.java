7a8672aefc8f6e1a159ecea1bce1e88df9f21a6a
Matt-Hoy <mattdhoy@gmail.com>
2025-03-08 16:13:33 -0600

    All Card Images complete

--- src/main/java/thenoble/cards/common/Untrustworthy.java
+++ src/main/java/thenoble/cards/common/Untrustworthy.java
@@ -11,15 +11,15 @@ import thenoble.cards.NobleCard;
 import thenoble.character.MyCharacter;
 import thenoble.util.CardStats;
 
-public class BuyThemADrink extends NobleCard {
-  public static final String ID = makeID("BuyThemADrink");
+public class Untrustworthy extends NobleCard {
+  public static final String ID = makeID("Untrustworthy");
   private static final CardStats INFO =
       new CardStats(
           MyCharacter.Meta.CARD_COLOR, CardType.SKILL, CardRarity.COMMON, CardTarget.ENEMY, 1);
   private static final int MAGIC = 1;
   private static final int UPG_MAGIC = 1;
 
-  public BuyThemADrink() {
+  public Untrustworthy() {
     super(ID, INFO);
 
     setMagic(MAGIC, UPG_MAGIC);
@@ -41,6 +41,6 @@ public class BuyThemADrink extends NobleCard {
 
   @Override
   public AbstractCard makeCopy() {
-    return new BuyThemADrink();
+    return new Untrustworthy();
   }
 }
