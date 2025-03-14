c1a3c195c2d628d318a8f52a1a9e0a81a0efb2a0
Matt-Hoy <mattdhoy@gmail.com>
2025-03-14 12:41:31 -0500

    Forgot to remove Confidence Test

--- src/main/java/thenoble/cards/test/ConfidenceTest.java
+++ src/main/java/thenoble/cards/test/ConfidenceTest.java
@@ -1,31 +1,31 @@
-package thenoble.cards.test;
-
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import thenoble.cards.NobleCard;
-import thenoble.character.MyCharacter;
-import thenoble.powers.ConfidencePower;
-import thenoble.util.CardStats;
-
-public class ConfidenceTest extends NobleCard {
-  public static final String ID = makeID("ConfidenceTest");
-  private static final CardStats INFO =
-      new CardStats(
-          MyCharacter.Meta.CARD_COLOR, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF, 0);
-
-  public ConfidenceTest() {
-    super(ID, INFO);
-  }
-
-  @Override
-  public void use(AbstractPlayer player, AbstractMonster monster) {
-    addToBot(new ApplyPowerAction(player, player, new ConfidencePower(player, 1)));
-  }
-
-  @Override
-  public AbstractCard makeCopy() {
-    return new ConfidenceTest();
-  }
-}
+// package thenoble.cards.test;
+//
+// import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+// import com.megacrit.cardcrawl.cards.AbstractCard;
+// import com.megacrit.cardcrawl.characters.AbstractPlayer;
+// import com.megacrit.cardcrawl.monsters.AbstractMonster;
+// import thenoble.cards.NobleCard;
+// import thenoble.character.MyCharacter;
+// import thenoble.powers.ConfidencePower;
+// import thenoble.util.CardStats;
+//
+// public class ConfidenceTest extends NobleCard {
+//  public static final String ID = makeID("ConfidenceTest");
+//  private static final CardStats INFO =
+//      new CardStats(
+//          MyCharacter.Meta.CARD_COLOR, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF, 0);
+//
+//  public ConfidenceTest() {
+//    super(ID, INFO);
+//  }
+//
+//  @Override
+//  public void use(AbstractPlayer player, AbstractMonster monster) {
+//    addToBot(new ApplyPowerAction(player, player, new ConfidencePower(player, 1)));
+//  }
+//
+//  @Override
+//  public AbstractCard makeCopy() {
+//    return new ConfidenceTest();
+//  }
+// }
